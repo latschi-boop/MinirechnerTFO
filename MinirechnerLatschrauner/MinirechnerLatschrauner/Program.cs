51e354a0a48d8120c2471ca3303f3892f1d04c11
filip latschrauner <02latfil@rgtfo-me.it>
2020-04-23 20:02:10 +0200

    Haupt

---
+++ MinirechnerLatschrauner/MinirechnerLatschrauner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinirechnerLatschrauner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Geben sie Zahl 1 ein");
+            int Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Geben sie Zahl 2 ein");
+            int Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
